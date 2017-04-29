8f49543e39e085e40ff53ac6bd58dbb6d4a25d61
Stefani Pereira <sutehanesilva@gmail.com>
2017-04-28 21:57:50 -0300

    Remove activities

--- activities/python-for-zombies/Cap 1 - Exercício 1.py
+++
@@ -1,5 +0,0 @@
-# Faça um programa que peça dois números inteiros e imprima a soma desses dois números
-
-a = int(input('Digite o primeiro número: '))
-b = int(input('Digite o segundo número: '))
-print (a + b)
--- activities/python-for-zombies/Cap 1 - Exercício 8.py
+++
@@ -1,6 +0,0 @@
-# Faça agora o contrário, de Fahrenheit para Celsius.
-
-fahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
-celsius = (fahrenheit - 32) * 1.8
-
-print ("A temperatura em Celsius é %.2f °C" %celsius)

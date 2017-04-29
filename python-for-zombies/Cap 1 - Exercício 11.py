e646cb0dfa4f7873d42ecf191b9c52d30375c9ea
Stefani Pereira <sutehanesilva@gmail.com>
2017-04-28 21:59:11 -0300

    Remove python-for-zombies

--- python-for-zombies/Cap 1 - Exercício 11.py
+++
@@ -1,4 +0,0 @@
-# Sabendo que str( ) converte valores numéricos para string, calcule quantos dígitos há em 2 elevado a um milhão.
-
-a = str(int(2**1000000))
-print ('A quantidade de dígitos é: %s' %len(a))
--- python-for-zombies/Cap 1 - Exercício 2.py
+++
@@ -1,6 +0,0 @@
-# Escreva um programa que leia um valor em metros e o exiba convertido em milímetros
-
-m = int(input('Metros: '))
-milímetros = m * 1000
-print (milímetros, 'metros, é equivalente a: %i' %r, 'milímetros')
-# poderia simplificar para print('Milímetros: ', m*1000)
